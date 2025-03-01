887e90e932325fb98273c6a8b95fc890bbc4ef19
Gabriel <yaman_gp@bancotoyota.com.br>
2025-03-01 19:30:12 -0300

    update project session

--- assets/js/load-page.js
+++ assets/js/load-page.js
@@ -1,27 +1,27 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const menu = document.getElementById("menu");
-//     const textElement = document.getElementById("load-page-text");
-//     const phrase = "ACAL ARQ";
-//     let currentIndex = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.getElementById("menu");
+    const textElement = document.getElementById("load-page-text");
+    const phrase = "ACAL ARQ";
+    let currentIndex = 0;
   
-//     // Função para adicionar letra por letra
-//     function typeLetter() {
-//       if (currentIndex < phrase.length) {
-//         textElement.innerHTML += phrase.charAt(currentIndex);
-//         currentIndex++;
-//         setTimeout(typeLetter, 400);  // Aguarda 200ms antes de adicionar a próxima letra
-//       } else {
-//         setTimeout(() => {
-//             document.querySelector('.load-page').classList.add('load-page-hidden');
-//             menu.style.display = 'flex';
-//         }, 3000);
-//       }
-//     }
+    // Função para adicionar letra por letra
+    function typeLetter() {
+      if (currentIndex < phrase.length) {
+        textElement.innerHTML += phrase.charAt(currentIndex);
+        currentIndex++;
+        setTimeout(typeLetter, 400);  // Aguarda 200ms antes de adicionar a próxima letra
+      } else {
+        setTimeout(() => {
+            document.querySelector('.load-page').classList.add('load-page-hidden');
+            menu.style.display = 'flex';
+        }, 3000);
+      }
+    }
   
-//     // Inicia a digitação da frase
-//     textElement.style.opacity = 1;
-//     typeLetter();
-//   });
+    // Inicia a digitação da frase
+    textElement.style.opacity = 1;
+    typeLetter();
+  });
   
 // Aba de Projetos
 let timer;
