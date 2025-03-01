659346478d33bd834b5b709c4509a625af11a2b9
Gabriel <yaman_gp@bancotoyota.com.br>
2025-03-01 19:56:16 -0300

    update project session

--- assets/js/load-page.js
+++ assets/js/load-page.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(() => {
             document.querySelector('.load-page').classList.add('load-page-hidden');
             menu.style.display = 'flex';
-        }, 3000);
+        }, 2000);
       }
     }
   
@@ -23,69 +23,3 @@ document.addEventListener("DOMContentLoaded", function() {
     typeLetter();
   });
   
-// Aba de Projetos
-let timer;
-
-if (window.matchMedia("(max-width: 768px)").matches) {
-  function checkVisibility() {
-      const img_academia = document.getElementById("projetos-imagem-academia");
-      const mensagem_academia = document.getElementById("projetos-mensagem-academia");
-      const rect = img_academia.getBoundingClientRect();
-      const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
-      
-      if (isVisible) {
-        if (!timer) {
-          timer = setTimeout(() => {
-            img_academia.style.opacity = "0.4";
-            mensagem_academia.style.opacity = "1";
-          }, 1500);
-        }
-      } else {
-        clearTimeout(timer);
-        timer = null;
-        img_academia.style.opacity = "1";
-        mensagem_academia.style.opacity = "0";
-      }
-
-      const img_comercial = document.getElementById("projetos-imagem-comercial");
-      const mensagem_comercial = document.getElementById("projetos-mensagem-comercial");
-      const rect_comercial = img_comercial.getBoundingClientRect();
-      const isVisible_comercial = rect_comercial.top >= 0 && rect_comercial.bottom <= window.innerHeight;
-      
-      if (isVisible_comercial) {
-        if (!timer) {
-          timer = setTimeout(() => {
-            img_comercial.style.opacity = "0.4";
-            mensagem_comercial.style.opacity = "1";
-          }, 1500);
-        }
-      } else {
-        clearTimeout(timer);
-        timer = null;
-        img_comercial.style.opacity = "1";
-        mensagem_comercial.style.opacity = "0";
-      }
-  
-      const img_residencial = document.getElementById("projetos-imagem-residencial");
-      const mensagem_residencial = document.getElementById("projetos-mensagem-residencial");
-      const rect_residencial = img_residencial.getBoundingClientRect();
-      const isVisible_residencial = rect_residencial.top >= 0 && rect_residencial.bottom <= window.innerHeight;
-      
-      if (isVisible_residencial) {
-        if (!timer) {
-          timer = setTimeout(() => {
-            img_residencial.style.opacity = "0.4";
-            mensagem_residencial.style.opacity = "1";
-          }, 1500);
-        }
-      } else {
-        clearTimeout(timer);
-        timer = null;
-        img_residencial.style.opacity = "1";
-        mensagem_residencial.style.opacity = "0";
-      }
-  }
-
-  window.addEventListener("scroll", checkVisibility);
-  window.addEventListener("load", checkVisibility);
-}
